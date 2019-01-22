717124404426e280a9fe0a701313ce9a5aab0775
SandrineLandsberg <46939688+SandrineLandsberg@users.noreply.github.com>
2019-01-22 20:48:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+    
+    
+    Im Urlaub mache ich gerne einen 
+    
+<?php 
+echo ($_GET['Unfug1']);
+echo $_GET["Unfug2"];
+echo $_GET["Unfug3"]?>!
+  
+    
+    
+    
+    </body></html>
---
+++ phrases.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+ 
+    <form action="index.php" method="get">
+        <h1> Im Urlaub mache ich gerne einen ...</h1>
+    
+        
+<select name="Unfug1" id="Unfug1">
+
+<option value="Cocktail">Cocktail</option>
+
+<option value="Yogakurs">Yogakurs</option>
+
+<option value="Auflauf">Auflauf</option>
+
+<option value="Ausflug">Ausflug</option>
+</select>
+
+ <select name="Unfug2">
+
+<option value="mit Brandy">mit Brandy</option>
+
+<option value="mit Fremden">mit Fremden</option>
+
+<option value="mit viel Käse">mit viel Käse</option>
+
+<option value="mit Musik">mit Musikt</option>
+</select>   
+    
+    
+ <select name="Unfug3">
+
+<option value="am Strand">am Strand</option>
+
+<option value="auf einem Leuchtturm">auf einem Leuchtturm</option>
+
+<option value="im Flugzeug">im Flugzeug</option>
+
+<option value="bei Sonnenuntergang">bei Sonnenuntergang</option>
+</select>   
+    
+<input type="submit">
+        
+    </form>
+    
+</body>
+</html>
